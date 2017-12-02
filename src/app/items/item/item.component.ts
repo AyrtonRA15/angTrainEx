6742f17968baded5ff0be80eb1db912693faed72
Ayrton Rojo <arojo@yuxiglobal.com>
2017-12-02 12:27:10 -0500

    img icon

--- src/app/items/item/item.component.ts
+++ src/app/items/item/item.component.ts
@@ -11,6 +11,8 @@ export class ItemComponent implements OnInit {
   @Input() item: IItem;
   @Output() onDelete = new EventEmitter<number>();
 
+  closeImgUrl = 'http://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/close-icon.png';
+
   constructor() { }
 
   ngOnInit() {
